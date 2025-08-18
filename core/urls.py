2ccf6e18c318e591925677d16d5b9bdb5a5680cb
victorgsanc1991 <victorgsanc1991@hotmail.com>
2025-08-18 14:57:43 +0200

    Feature: Implementa flujo de login unificado y plantilla base

--- core/urls.py
+++ core/urls.py
@@ -2,14 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.subir_movimientos, name='subir'),
+    # La página raíz ahora apunta al dashboard, que actuará como página de inicio
+    path('', views.dashboard, name='dashboard'),
+    
+    # Las otras páginas tienen sus propias URLs
+    path('subir/', views.subir_movimientos, name='subir'),
     path('transacciones/', views.listar_transacciones, name='listar'),
     path('actualizar-categoria/', views.actualizar_categoria, name='actualizar_categoria'),
-    path('dashboard/', views.dashboard, name='dashboard'),
-
-    # --- AÑADE ESTAS DOS LÍNEAS ---
-    # URL para borrar una sola transacción. Capturamos el ID desde la URL.
     path('transaccion/<int:transaccion_id>/eliminar/', views.eliminar_transaccion, name='eliminar_transaccion'),
-    # URL para procesar la eliminación múltiple.
     path('transacciones/eliminar-multiples/', views.eliminar_multiples, name='eliminar_multiples'),
 ]
